ae4776670081cb91e9361c39a4e29fd37638df79
Davidson Fellipe <email@fellipe.com>
2012-11-11 21:07:40 -0200

    adicao de modulo dos certificados

---
+++ certificado/certificado-miolo.php
@@ -0,0 +1,98 @@
+  <!--[if lt IE 7]>
+      <p class="chromeframe">Come on! Você está usando um navegador tão antigo... Que pelas barbas de Merlin! <a href="http://browsehappy.com/">Atualize agora mesmo</a> ou <a href="http://www.google.com/chromeframe/?redirect=true">apenas instale o Google Chrome Frame</a> para uma melhor experiência nesse e em outros sites. \m/</p>
+  <![endif]-->
+
+  <div id="wrapper">
+    <header class="container">
+      <hgroup>
+        <h1><a href="/"><img src="../img/logo.png" alt="Front in Maceió" class="logo" width="193" height="83"></a></h1>
+        <h2><strong><time itemprop="startDate" datetime="2012-10-27">27 de out</time></strong> CESMAC - Maceió
+          <a class="url-certificado" href="certificado">&#9733; gere seu certificado aqui &#9733;</a>
+        </h2>
+      </hgroup>
+      <div class="social-media">
+        <a href="http://fb.com/frontinmaceio" rel="external" class="fb">Front in Maceió on Facebook</a>
+        <a href="http://twitter.com/frontinmaceio" rel="external" class="twitter">Front in Maceió on Twitter</a>
+        <a href="mailto:hey@frontinmaceio.com.br?Subject=Front%20in%20Maceió%20%2B%20Contato" rel="external" class="mail">Dúvidas, críticas ou sugestões</a>
+      </div>
+      <ul class="share">
+          <li class="twitter">
+              <a href="https://twitter.com/frontinmaceio" class="twitter-follow-button" data-show-count="false" data-lang="pt" data-show-screen-name="false">Seguir @frontinmaceio</a>
+          </li>
+          <li class="twitter">
+              <a href="https://twitter.com/share" class="twitter-share-button" data-text="Eu vou para o @frontinmaceio 2012 no dia 27 de Outubro em Maceió!" data-count="none" data-related="frontinmaceio" data-dnt="true" data-lang="pt-BR">Tweet</a>
+          </li>
+          <li class="facebook">
+              <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Ffacebook.com/frontinmaceio&amp;send=false&amp;layout=button_count&amp;width=80&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;appId=326273014110834" style="border:none; overflow:hidden; width:80px; height:21px;"></iframe>
+          </li>
+          <li class="plusone">
+            <div class="g-plusone" data-size="medium"></div>
+          </li>
+        </ul>
+    </header>
+
+    <form action="certificado-gerado" method="POST" class="gerador-certificado">
+        <input type="email" name="name" Placeholder="Digite seu email" required>
+        <input type="submit" value="Gerar Certificado" class="submit">
+    </form>
+    <footer id="footer">
+    <div role="main" class="main">
+        <section id="organization">
+          <h4>Organização</h4>
+          <ul>
+            <li>
+              <a href="http://www.braziljs.org" rel="external">
+                <img src="../img/organization/braziljs.jpg" alt="BrazilJS Foundation logo" width="118" height="44">
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        <section id="trustees" class="responsible">
+          <h4>Curadores</h4>
+          <ul>
+            <li>
+              <span>Davidson Fellipe</span>
+              <a href="http://github.com/davidsonfellipe">davidsonfellipe</a>
+            </li>
+            <li>
+              <span>Juarez P. A. Filho</span>
+              <a href="http://github.com/juarezpaf">juarezpaf</a>
+            </li>
+          </ul>
+        </section>
+        <section id="designer" class="responsible">
+          <h4>Designer</h4>
+          <ul>
+            <li>
+              <span>Filipe Guth</span>
+              <a href="http://github.com/fguth">fguth</a>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </footer>
+  </div>
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
+  <script src="../js/plugins.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+  <script src="../js/main.js"></script>
+
+  <script>
+    var _gaq=[['_setAccount','UA-34840693-1'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+
+    window.___gcfg = {lang: 'pt-BR'};
+
+    (function() {
+      var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+      po.src = 'https://apis.google.com/js/plusone.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    })();
+
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+  </script>
